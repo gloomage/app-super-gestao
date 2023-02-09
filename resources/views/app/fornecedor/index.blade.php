182e0e15478e1f33dd083217e7bc26c3e0652cc7
gloomage <carlos.mnsp@gmail.com>
2023-02-09 18:00:23 -0300

    bcp

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,5 @@
+<h3>Fornecedor</h3>
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::get(
-    '/contato/{nome}/{categoria}/{assunto}/{mensagem}', 
-    function(string $nome, string $categoria, string $assunto, string $mensagem){
-        echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
-    }
-);
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Cliente';})->name('app.clientes');
+    Route::get('/fornecedores', [\App\Http\Controllers\FornecedorController::class, 'index'])->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
+});
+
+Route::get('/teste/{p1}/{p2}',  [\App\Http\Controllers\TesteController::class, 'teste'])->name('teste');
+
+Route::fallback(function(){
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'"> clique  aqui </a> para ir para página inicial';
+});
