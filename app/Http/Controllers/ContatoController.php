da6b670fe7b3652e30d4c24d25792ee5c0f39845
gloomage <carlos.mnsp@gmail.com>
2023-02-15 17:54:15 -0300

    feat: contact form working correctly

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
+use App\Models\MotivoContato;
 
 class ContatoController extends Controller
 {
-    public function contato()
+    public function contato(Request $request)
     {
-        return view('site.contato', ['titulo' => 'Contato']);
+        $motivo_contatos = MotivoContato::all();
+
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
+    }
+
+    public function salvar(Request $request) 
+    {
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'telefone' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
+            'mensagem' => 'required|max:2000'
+        ];
+        $feedback = [
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+
+            'email.email' => 'O e-mail informado não é válido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+        $request->validate($regras, $feedback);
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
